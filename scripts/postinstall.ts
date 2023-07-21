a1ff052979889d2d149a51db8d323088fad5aa9b
jayromberg <jayro.dev@gmail.com>
2023-07-21 13:00:01 -0300

    feat: atualiza script `postinstall`

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -14,3 +14,17 @@ try {
     "[INSTALL FRONTEND]: Finished installing frontend's dependencies.",
   );
 }
+
+console.log("[INSTALL BACKEND]: Installing backend's dependencies...");
+
+try {
+  const commandBackend = "cd backend && yarn install";
+  execSync(commandBackend, { encoding: "utf8" });
+} catch (error) {
+  console.log(error);
+} finally {
+  console.log("[INSTALL BACKEND]: Finished installing backend's dependencies.");
+}
+
+console.log("========================================");
+console.log("[INSTALL]: FINISHED");
