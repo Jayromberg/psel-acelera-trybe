15f51763e25b921171ffacfbfe653e52eaa1d4d9
jayromberg <jayro.dev@gmail.com>
2023-07-21 12:47:54 -0300

    refactor: adiciona doretorio src em frontend

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -6,8 +6,7 @@ console.log("[INSTALL FRONTEND]: Installing frontend's dependencies...");
 
 try {
   const commandFrontend = "cd frontend && yarn install";
-  const outputFrontend = execSync(commandFrontend, { encoding: "utf8" });
-  console.log(outputFrontend);
+  execSync(commandFrontend, { encoding: "utf8" });
 } catch (error) {
   console.log(error);
 } finally {
